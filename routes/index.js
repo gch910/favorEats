58afbd59f7db37d2e7ab18f42ba8a12c8fd1a0a6
Gabriel Harris <gch910@gmail.com>
2021-02-02 19:29:33 -0600

    figured out index.js query for restaurants/user

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,22 @@ router.get(
   asyncHandler(async (req, res, next) => {
     // console.log(req.session);
     const user = req.session.auth.userId;
-    const visited = await db.Restaurant.findAll({
-      include: db.User,
+    // want to be querying into user and not restaurant - because we have the user id from req.session.auth
+    const visited = await db.User.findAll({
+      include: db.Restaurant,
       //not sure about everything below!
       where: {
         id: user,
       },
     });
-    // console.log(visited[0].VisitedRestaurant);
+    //needed to console.log to see object so we know what to key into
+    const restaurants = visited[0].Restaurants
+    
+    // console.log(visited[0].Restaurants.name);
 
     res.render("index", {
       title: "a/A Express Skeleton Homes",
-      visited,
+      restaurants
     });
   })
 );
