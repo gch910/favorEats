2831038b2280ff80b8e364994ccc69929d53c06a
Gabriel Harris <gch910@gmail.com>
2021-02-07 18:33:37 -0600

    still trying to get top restaurants

--- routes/index.js
+++ routes/index.js
@@ -18,24 +18,52 @@ router.get(
     });
 
     const topRestaurants = await db.Rating.findAll({
-      include: db.Restaurant,
       where: {
         rating: [4, 5]
 
       }
     })
-    const highestRated = topRestaurants
+
+    const restaurants = await db.Restaurant.findAll();
+
+    const topRatingsId = topRestaurants.map(rating => rating.restaurantId)
+    const filteredTopIds = topRatingsId.filter((id, i) => {
+      return topRatingsId.indexOf(id) === i;
+    })
+
+    
+   
     // console.log(topRestaurants[0])
     const currentUser = visited[0]
     const visitedRestaurants = visited[0].visited
     const wantToVisit = visited[0].Restaurants
-   
+    
+    // visitedRestaurants.forEach(visited => console.log(visited.name))
+    const visitedRendered = [];
+    const wantToVisitRendered = []
+    const topRendered = [];
+    for(let i = 0; i < visitedRestaurants.length && i < 3; i++) {
+      visitedRendered.push(visitedRestaurants[i])
+    }
+
+    for(let i = 0; i < wantToVisit.length && i < 3; i++) {
+      wantToVisitRendered.push(wantToVisit[i])
+    }
+    
+    for(let i = 0; i < restaurants.length && i < 3; i++) {
+      if(filteredTopIds.includes(restaurants[i].id)) {
+        topRendered.push(restaurants[i])
+      }
+    }
+
+    console.log(topRendered)
+    
 
     res.render("index", {
       currentUser,
-      visitedRestaurants,
-      wantToVisit,
-      highestRated
+      visitedRendered,
+      wantToVisitRendered,
+      topRendered,
     });
   })
 );
