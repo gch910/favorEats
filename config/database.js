d1401a1ca070f70685d737fdb819dc917d1176cb
Gabriel Harris <gch910@gmail.com>
2021-05-11 19:08:23 -0500

    test heroku

--- config/database.js
+++ config/database.js
@@ -15,7 +15,8 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     "dialectOptions": {
-      "ssl": true
+      "ssl": true,
+      rejectUnauthorized: false
     },
     seederStorage: 'sequelize',
   }
